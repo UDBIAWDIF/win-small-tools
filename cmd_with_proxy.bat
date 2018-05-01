b7b7fec3f7f6686903e8d2583d0a5781defb1a24
uid@yeah.net <uid@yeah.net>
2018-05-01 20:58:29 +0800

    update

---
+++ cmd_with_proxy.bat
@@ -0,0 +1,2 @@
+start cmd /k "set http_proxy=http://127.0.0.1:1080/pac?auth=RMQwdbKKeff00Q5NzOks^&t=201801031758473199&&set https_proxy=https://127.0.0.1:1080/pac?auth=RMQwdbKKeff00Q5NzOks^&t=201801031758473199"
+
