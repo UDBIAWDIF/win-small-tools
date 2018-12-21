9e0669cfeead53d8104d7bdebd184d1bda0e68f7
UID@YEAH.NET <UID@YEAH.NET>
2018-12-21 10:31:15 +0800

    Just update.

--- cmd_with_proxy.bat
+++ cmd_with_proxy.bat
@@ -1,2 +1,1 @@
-start cmd /k "set http_proxy=http://127.0.0.1:1080/pac?auth=RMQwdbKKeff00Q5NzOks^&t=201801031758473199&&set https_proxy=https://127.0.0.1:1080/pac?auth=RMQwdbKKeff00Q5NzOks^&t=201801031758473199"
-
+start cmd /k "set http_proxy=http://127.0.0.1:1080/pac?auth=jp5MvcwSwU2jcOwqMYDF^&t=201812211013211369&&set https_proxy=http://127.0.0.1:1080/pac?auth=jp5MvcwSwU2jcOwqMYDF^&t=201812211013211369"
